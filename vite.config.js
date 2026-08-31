a1777298f3082bef1a76c6df5c6608a4fee9f521
Shawn <qianszn@gmail.com>
2026-09-01 00:08:23 +0800

    config fix base

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/",
+  base: '/portfolio/', // 仓库名称，两边都要带斜杠
 })
-
